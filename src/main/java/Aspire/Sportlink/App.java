62dde5f30bffb5697d71ecd9c8e395220c35e3d7
rajkumaryadav <rajkumar.rvyadav@gmail.com>
2024-03-07 20:14:53 +0530

    second commit

---
+++ src/main/java/Aspire/Sportlink/App.java
@@ -0,0 +1,22 @@
+package Aspire.Sportlink;
+
+import org.testng.annotations.Test;
+
+public class App{
+	
+	@Test
+public void demo()
+{
+	System.out.println("Hello world");
+}
+	@Test
+public void demo1()
+{
+	System.out.println("I am a indian tester");
+}
+	@Test
+public void demo2()
+{
+	System.out.println("can you give a job for me");
+}
+}
